b11fc07d7b28fe8bb3f46e94b872fde9a8dfe316
MAFAZ9416 <mafaz9416@gmail.com>
2026-05-13 16:38:45 +0530

    skill bar update

--- src/App.jsx
+++ src/App.jsx
@@ -52,7 +52,7 @@ function App() {
           </div>
 
           <p className="mt-6 text-lg font-display font-semibold text-gradient">
-            MOHAMMED MAFAZ
+            PORTFOLIO.
           </p>
 
         </div>
